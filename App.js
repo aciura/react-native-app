bd71acc5e9e1e3ddfaefef76205354150fed0e48
Adrian Ciura <aciura@gmail.com>
2018-09-01 13:30:16 +0200

    [#2.1] Add simple form for adding new note

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput, Switch, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Switch, ScrollView, AsyncStorage } from 'react-native';
 import { Constants } from 'expo';
+
+const storageKey = 'cloudtech:notesApp';
 
 export default class App extends React.Component {
   state = {
+    value: '',
+    actions: [],
     notes: [ 
-      {title:'note1', text:'text of note1', done:false, timestamp:Date.now(),}, 
-      {title:'note2', text:'text of note2. very interesting', done:false, timestamp:Date.now()},
-      {title:'note3', text:'this is the text of note 3', done:true, timestamp:Date.now(),},
+      {title:'note1', text:'text of note1', done:false, timestamp:new Date().getTime(),}, 
+      {title:'note2', text:'text of note2. very interesting', done:false, timestamp:new Date().getTime()},
+      {title:'note3', text:'this is the text of note 3', done:true, timestamp:new Date().getTime(),},
     ]
   }
+
+  // componentWillMount() {
+  //   AsyncStorage.ge
+  // }
 
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.container}>
+          <TextInput
+            placeholder="Enter text"
+            returnKeyType="done"
+            value={this.state.value}
+            onChangeText={this.textChanged}
+            onSubmitEditing={this.submit}
+            
+          />
+          <View style={styles.container}>
+            {this.state.actions.map(({ timestamp, type, value }) => (
+              <Text key={timestamp}>
+                <Text style={{ fontWeight: 'bold' }}>{type}</Text>
+                <Text>{JSON.stringify(value)}</Text>
+              </Text>
+            ))}
+          </View>
+        </View>
+
         <ScrollView style={styles.scrollView}>
         {this.state.notes.map((note)=>(
           <View key={note.title} style={styles.item}>                      
@@ -31,6 +58,30 @@ export default class App extends React.Component {
       </View>
     );
   }
+  
+  textChanged = text =>
+    this.setState(state => ({
+      value: text,
+      actions: state.actions.concat({
+        timestamp: new Date().getTime(),
+        type: 'TEXT_CHANGED',
+        value: text,
+      }),
+    }));
+
+  submit = () =>
+    this.setState(state => ({
+      actions: state.actions.concat({
+        timestamp: new Date().getTime(),
+        type: 'TEXT_SUBMIT',
+      }),
+      notes: state.notes.concat({
+        title:`note${state.notes.length + 1}`, 
+        text:state.value, 
+        done:false, 
+        timestamp:new Date().getTime(),
+      }),
+    }));
 
   noteDoneChanged = (value, note) => {
     console.log(`noteDoneChanged:${note.title}, value:${value}`)
